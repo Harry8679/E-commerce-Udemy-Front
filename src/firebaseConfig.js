e78e120eef5e17f6f7d93893a31a0d3194127bec
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-13 13:49:40 +0100

    Useless comment

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,4 @@
 import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
@@ -13,18 +10,6 @@ const firebaseConfig = {
   appId: "1:922175331675:web:4b54bcc772a9a8106f0e7e",
   measurementId: "G-2WVRKSH5NF"
 };
-// const firebaseConfig = {
-//   apiKey: "VOTRE_API_KEY",
-//   authDomain: "VOTRE_AUTH_DOMAIN",
-//   projectId: "VOTRE_PROJECT_ID",
-//   storageBucket: "VOTRE_STORAGE_BUCKET",
-//   messagingSenderId: "VOTRE_MESSAGING_SENDER_ID",
-//   appId: "VOTRE_APP_ID",
-// };
-
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-// export const db = getFirestore(app);
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
